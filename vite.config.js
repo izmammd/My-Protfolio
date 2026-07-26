99e76dd223c289ed0b5ec397a3dfb0866a445e25
izmammd <izmammd763@gmail.com>
2026-07-27 05:03:17 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Portfolio/',
+  base: '/',
   plugins: [react()],
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/My-Portfolio/',
+//   plugins: [react()],
+// })
